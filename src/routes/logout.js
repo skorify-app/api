e87e147652b2215b4a4cf94cd99d9257366d68ef
Alldreno Hosea <Alldreno.4342401059@students.polibatam.ac.id>
2025-10-22 09:25:49 +0700

    Fix error message

--- src/routes/logout.js
+++ src/routes/logout.js
@@ -7,7 +7,7 @@ export default async(c, db, util) => {
     const body = await c.req.json();
 
     if (!util.validate.body(requiredData, body)) {
-      return await util.error(c, 400, 'Maaf, terdapat kesalahan saat mencoba keluar akun.1');
+      return await util.error(c, 400, 'Maaf, terdapat kesalahan saat mencoba keluar akun.');
     }
 
     const { sessionId } = body;
@@ -15,14 +15,14 @@ export default async(c, db, util) => {
 
     console.log(sessionId.length)
     if (!util.validate.sessionId(sessionId)) {
-      return await util.error(c, 400, 'Maaf, terdapat kesalahan saat mencoba keluar akun.2');
+      return await util.error(c, 400, 'Maaf, terdapat kesalahan saat mencoba keluar akun.');
     }
 
     await db.session.remove(conn, sessionId);
     return c.json({ success: true });
   } catch(err) {
     console.error(err);
-    return await util.error(c, 500, 'Maaf, terdapat kesalahan saat mencoba keluar akun.3');
+    return await util.error(c, 500, 'Maaf, terdapat kesalahan saat mencoba keluar akun.');
   } finally {
     if (conn) conn.release();
   }
