e015ab11ec48f8f3f8ded4dc21e405f5024d81da
Alldreno Hosea <Alldreno.4342401059@students.polibatam.ac.id>
2026-01-05 13:41:41 +0700

    Sort the scores

--- src/database/all-scores.js
+++ src/database/all-scores.js
@@ -1,3 +1,3 @@
 export const get = async(conn, accountId) => {
-	return await conn.query('SELECT * FROM AllScores WHERE account_id = ?;', [accountId]);
+	return await conn.query('SELECT * FROM AllScores WHERE account_id = ? ORDER BY recorded_at ASC;', [accountId]);
 }
